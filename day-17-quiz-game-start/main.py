a0dc302fa4123ebcf1fb1fe4963149cd913c4612
andrew <greensweight96@gmail.com>
2023-07-21 16:41:54 -0700

    Finished day 17 of 100 days of Python

--- day-17-quiz-game-start/main.py
+++ day-17-quiz-game-start/main.py
@@ -6,10 +6,16 @@ question_bank = []
 
 
 for question in question_data:
-    new_q = Question(question["text"], question["answer"])
-    print(f"The text is {new_q.text} and the answer is {new_q.answer}")
+    new_q = Question(question["question"], question["correct_answer"])
+    # print(f"The text is {new_q.text} and the answer is {new_q.answer}")
     question_bank.append(new_q)
 
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+# quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
