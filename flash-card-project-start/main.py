489f935bd87cbf0baf0672486c35a3641464c989
andrew <greensweight96@gmail.com>
2023-09-04 21:50:45 -0700

    Finished the capstone project, it involved tkinter and pandas

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -1,20 +1,81 @@
 from tkinter import *
 from tkinter import messagebox
+import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+# Convert the DataFrame to a list of dictionaries ‘records’ : list like [{column -> value}, … , {column -> value}]
+to_learn = {}
+
+# A blank dictionary then convert to a global variable to get updated
+current_card = {}
+
+try:
+    data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient='records')
+else:
+    to_learn = data.to_dict(orient='records')
 
 
+#--------------------------Next card-------------------------
+def next_card():
+    # Pick a random dictionary from result_list
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    
+    # Update the canvas with the French word
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
+
+#--------------------------Flip Card--------------------------------------
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")    
+    canvas.itemconfig(card_background, image=card_back_img)
+
+#--------------------------Remove Known Cards----------------------------
+def is_known():
+    to_learn.remove(current_card)
+
+    data = pd.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+
+    next_card()
 #--------------------------UI Setup---------------------------------------
 window = Tk()
 window.title("Flashy")
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
-logo_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=logo_img)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+canvas.config(bg=BACKGROUND_COLOR)
 canvas.grid(row=0, column=0, columnspan=2)
+card_title = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 
-# Add the x button
+
+# Add the x (wrong) button
+x_image = PhotoImage(file="images/wrong.png")
+x_btn = Button(image=x_image, highlightthickness=0, command=next_card)
+x_btn.grid(row=1, column=0)
+
+# Add the check (right) button
+right_image = PhotoImage(file="images/right.png")
+right_btn = Button(image=right_image, highlightthickness=0, command=is_known)
+right_btn.grid(row=1, column=1)
+
+
+next_card()
+
 
 window.mainloop()
